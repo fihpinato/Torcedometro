a8b98ae3f6b70316b6739552df509e69f76a01f6
logonrm <logonrm@fiap.com.br>
2017-09-22 09:33:58 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/torcedometro/MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.logonrm.torcedometro;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private final String LOGIN_DEFAULT="android";
+    private final String SENHA_DEFAULT="123";
+
+    private final String KEY_APP_PREFERENCES = "login";
+    private String KEY_LOGIN = "login";
+
+    private TextInputLayout tilLogin;
+    private TextInputLayout tilSenha;
+    private CheckBox cbManterConectado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        tilLogin = (TextInputLayout) findViewById(R.id.tilLogin);
+        tilSenha = (TextInputLayout) findViewById(R.id.tilSenha);
+        cbManterConectado = (CheckBox) findViewById(R.id.cbManterConectado);
+        
+        if(isConectado())
+            iniciarApp();
+    }
+
+    public void logar(View v){
+        if(isLoginValido()){
+            if(cbManterConectado.isChecked()){
+                manterConectado();
+            }
+            iniciarApp();
+        }
+    }
+
+    private void manterConectado() {
+        String login = tilLogin.getEditText().getText().toString();
+        SharedPreferences pref = getSharedPreferences(KEY_APP_PREFERENCES, MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString(KEY_LOGIN, login);
+        editor.apply();
+    }
+
+    private boolean isLoginValido() {
+        String login = tilLogin.getEditText().getText().toString();
+        String senha = tilSenha.getEditText().getText().toString();
+        
+        if(login.equals(LOGIN_DEFAULT) && senha.equals(SENHA_DEFAULT)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void iniciarApp() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    private boolean isConectado() {
+        SharedPreferences shared = getSharedPreferences("info", MODE_PRIVATE);
+        String login = shared.getString(KEY_LOGIN, "");
+        if(login.equals(""))
+            return false;
+        else
+            return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
